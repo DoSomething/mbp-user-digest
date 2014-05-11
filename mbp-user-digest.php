6376db9188e849380266d575b9cbaa64c93614d8
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2014-05-10 23:17:42 -0400

    First draft

---
+++ mbp-user-digest.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * mbp-user-digest.php
+ *
+ * A producer to create entries in the userDigestQueue via the directUserDigest
+ * exchange. The mbc-user-digest application will consume the entries in the
+ * queue.
+ */
+
+// Load up the Composer autoload magic
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Load configuration settings common to the Message Broker system
+// symlinks in the project directory point to the actual location of the files
+require __DIR__ . '/mb-secure-config.inc';
+require __DIR__ . '/mb-config.inc';
+
+require __DIR__ . '/MBC_UserDigest.class.inc';
+
+// Settings
+$credentials = array(
+  'host' =>  getenv("RABBITMQ_HOST"),
+  'port' => getenv("RABBITMQ_PORT"),
+  'username' => getenv("RABBITMQ_USERNAME"),
+  'password' => getenv("RABBITMQ_PASSWORD"),
+  'vhost' => getenv("RABBITMQ_VHOST"),
+);
+$config = array(
+  'exchange' => array(
+    'name' => getenv("MB_USER_DIGEST_EXCHANGE"),
+    'type' => getenv("MB_USER_DIGEST_EXCHANGE_TYPE"),
+    'passive' => getenv("MB_USER_DIGEST_EXCHANGE_PASSIVE"),
+    'durable' => getenv("MB_USER_DIGEST_EXCHANGE_DURABLE"),
+    'auto_delete' => getenv("MB_USER_DIGEST_EXCHANGE_AUTO_DELETE"),
+  ),
+  'queue' => array(
+    array(
+      array(
+        'name' => getenv("MB_USER_DIGEST_QUEUE"),
+        'passive' => getenv("MB_USER_DIGEST_QUEUE_PASSIVE"),
+        'durable' => getenv("MB_USER_DIGEST_QUEUE_DURABLE"),
+        'exclusive' => getenv("MB_USER_DIGEST_QUEUE_EXCLUSIVE"),
+        'auto_delete' => getenv("MB_USER_DIGEST_QUEUE_AUTO_DELETE"),
+        'bindingKey' => getenv("MB_USER_DIGEST_QUEUE_TOPIC_MB_TRANSACTIONAL_EXCHANGE_PATTERN"),
+      ),
+    ),
+  ),
+);
+$settings = array(
+  'stathat_ez_key' => getenv("STATHAT_EZKEY"),
+);
+
+echo '------- mbp-user-digest START: ' . date('D M j G:i:s T Y') . ' -------', "\n";
+
+// Kick off
+$mbpUserDigest = new MBP_UserDigest($credentials, $config, $settings);
+
+// Gather digest message mailing list
+$mbpUserDisgest->produceUserDigestQueue();
+
+echo '------- mbp-user-digest END: ' . date('D M j G:i:s T Y') . ' -------', "\n";
