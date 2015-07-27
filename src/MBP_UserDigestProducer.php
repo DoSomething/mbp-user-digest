2465ea0d86c1d1852a89a63b0f779375ca4b67a4
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-07-27 00:05:21 -0400

    Add generatePayload() method

--- src/MBP_UserDigestProducer.php
+++ src/MBP_UserDigestProducer.php
@@ -3,17 +3,18 @@
  * MBP_UserDigestProducer - Static class of methods to create "snapshot" of mb-user collection. Generate
  * queue entries in userDigestProducerQueue to manage paged calls to mb-user collection.
  */
-namespace DoSomething\MBC_UserDigest;
+namespace DoSomething\MBP_UserDigest;
 
 use DoSomething\StatHat\Client as StatHat;
 use DoSomething\MB_Toolbox\MB_Toolbox;
-use DoSomething\MB_Toolbox\MB_Toolbox_BaseProducer;
+// Adjust path when BaseProducer is moved into MB_Toolbox library
+use DoSomething\MBP_UserDigest\MB_Toolbox_BaseProducer;
 
 /*
  * MBC_UserAPICampaignActivity.class.in: Used to process the transactionalQueue
  * entries that match the campaign.*.* binding.
  */
-class MBC_UserDigestProducer extends MB_Toolbox_BaseProducer
+class MBP_UserDigestProducer extends MB_Toolbox_BaseProducer
 {
 
   // The number of user documents to collect in a singe page request to /users
@@ -34,7 +35,7 @@ class MBC_UserDigestProducer extends MB_Toolbox_BaseProducer
 
   /**
    * Create entries in userDigestProducerQueue for each of the paged queries to make
-   * to mb-user-api. Additional consumers of the queue will increate the rate that
+   * to mb-user-api. Additional consumers of the queue will increase the rate that
    * the user data for digest generation will be prepared for consumption by
    * mbc-digest-email.
    */
@@ -46,7 +47,8 @@ class MBC_UserDigestProducer extends MB_Toolbox_BaseProducer
     do {
       $pageCount++;
       $usersPagedURL = self::generatePageRequestsURL($pageCount);
-      self::produceQueue($usersPagedURL);
+      $payload = self::generatePayload();
+      parent::produceQueue($payload, 'userDigestProducerQueue');
 
     } while ($pageCount > $totalPages);
 
@@ -58,7 +60,7 @@ class MBC_UserDigestProducer extends MB_Toolbox_BaseProducer
    * @return integer $totalPages
    *   The total number of pages.
    */
-  static public function gatherTotalPages() {
+  static private function gatherTotalPages() {
 
     // Request the total number of user documents that have campaign activity
     $totalDocuments = '';
@@ -76,7 +78,7 @@ class MBC_UserDigestProducer extends MB_Toolbox_BaseProducer
    * @return string $usersPagedURL
    *   The URL to request a page of user documents.
    */
-  static public function generatePageRequestsURL($page) {
+  static private function generatePageRequestsURL($page) {
     
     $curlUrl = $this->settings['ds_user_api_host'];
     $port = $this->settings['ds_user_api_port'];
@@ -87,25 +89,25 @@ class MBC_UserDigestProducer extends MB_Toolbox_BaseProducer
     $usersPagedURL = $curlUrl . '/users?page=' . $pageCount . '&pageSize=' . self::PAGE_SIZE . '&excludeNoCampaigns=1';
     return $usersPagedURL;
   }
-
   
   /**
-   * produceQueue: Add message to userDigestProducerQueue
+   * generatePayload: Format message payload
    *
    * @param string $usersPagedURL
    *   URL to add to message payload
+   * @param string $routingKey
+   *   The key to send with message to determine which queues bound to the
+   *   exchange will receive the message.
    */
-  static public function produceQueue($usersPagedURL) {
+  public function generatePayload($usersPagedURL, $routingKey = 'userDigestProducerQueue') {
 
-    // @todo: Use common message formatter for all producers and consumers in Message Broker system.
+    parent::generatePayload($usersPagedURL, $routingKey);
+
+    // @todo: Use common message formatted for all producers and consumers in Message Broker system.
     // Ensures consistent message structure.
-    $payload = array(
-      'url' => $usersPagedURL,
-      'requested' => time(),
-      'startTime' => $this->startTime,
-    );
+    $payload['url'] = $usersPagedURL;
 
-    $routingKey = 'userDigestDirectorQueue';
-    parent::produceQueue($payload, $routingKey);
+    return $payload;
   }
+
 }
