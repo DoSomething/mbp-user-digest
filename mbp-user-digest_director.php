4cff01552c059d98d01deb9c8da738ae557b2a25
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-08-14 18:52:19 -0400

    Functional director kickoff as daemon

--- mbp-user-digest_director.php
+++ mbp-user-digest_director.php
@@ -12,26 +12,15 @@ define('CONFIG_PATH',  __DIR__ . '/messagebroker-config');
 
 // Load up the Composer autoload magic
 require_once __DIR__ . '/vendor/autoload.php';
-use DoSomething\MBP_UserDigest\MBP_UserDigestDirector;
-
-use DoSomething\StatHat\Client as StatHat;
-use DoSomething\MB_Toolbox\MB_Toolbox;
+use DoSomething\MBP_UserDigest\MBP_UserDigest_Director;
 
 require_once __DIR__ . '/mbp-user-digest_director.config.inc';
 
-// Create objects for injection into MBC_ImageProcessor
-$mb = new MessageBroker($credentials, $config);
-$sh = new StatHat([
-  'ez_key' => $settings['stathat_ez_key'],
-  'debug' => $settings['stathat_disable_tracking']
-]);
-$tb = new MB_Toolbox($settings);
 
-
-echo '------- mbp-user-digest START: ' . date('D M j G:i:s T Y') . ' -------', PHP_EOL;
+echo '------- mbp-user-digest_director START: ' . date('D M j G:i:s T Y') . ' -------', PHP_EOL;
 
 // Kick off
-$mbpUserDigestDirector = new MBP_UserDigestDirector($mb, $sh, $tb, $settings);
-$mbpUserDigestDirector->producer();
+$mb = $mbConfig->getProperty('messageBroker');
+$mb->consumeMessage(array(new MBP_UserDigest_Director(), 'consumeDigestProducerQueue'));
 
-echo '------- mbp-user-digest END: ' . date('D M j G:i:s T Y') . ' -------', PHP_EOL;
+echo '------- mbp-user-digest_director END: ' . date('D M j G:i:s T Y') . ' -------', PHP_EOL;
