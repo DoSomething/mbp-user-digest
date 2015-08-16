66e7e2e97adedcd52158c39a483fef6013a56e18
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-08-15 22:33:50 -0400

    Point to MBP_UserDigest_DirectorConsumer class

--- mbp-user-digest_director.php
+++ mbp-user-digest_director.php
@@ -12,7 +12,7 @@ define('CONFIG_PATH',  __DIR__ . '/messagebroker-config');
 
 // Load up the Composer autoload magic
 require_once __DIR__ . '/vendor/autoload.php';
-use DoSomething\MBP_UserDigest\MBP_UserDigest_Director;
+use DoSomething\MBP_UserDigest\MBP_UserDigest_DirectorConsumer;
 
 require_once __DIR__ . '/mbp-user-digest_director.config.inc';
 
@@ -21,6 +21,6 @@ echo '------- mbp-user-digest_director START: ' . date('D M j G:i:s T Y') . ' --
 
 // Kick off
 $mb = $mbConfig->getProperty('messageBroker');
-$mb->consumeMessage(array(new MBP_UserDigest_Director(), 'consumeDigestProducerQueue'));
+$mb->consumeMessage(array(new MBP_UserDigest_DirectorConsumer(), 'consumeDigestProducerQueue'));
 
 echo '------- mbp-user-digest_director END: ' . date('D M j G:i:s T Y') . ' -------', PHP_EOL;
