aa6adb787b2525ad7d04591e9e50f69ac083beef
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-08-18 19:53:18 -0400

    Funcitonal DirectorProducer

--- src/MBP_UserDigest_DirectorProducer.php
+++ src/MBP_UserDigest_DirectorProducer.php
@@ -53,40 +53,50 @@ class MBP_UserDigest_DirectorProducer extends MBP_UserDigest_BaseProducer
    */
   public function setUser($user) {
 
-    // @todo: Add email format validation
-    if ($this->isValidEmail($user->email)) {
-      $this->digestUser['email'] = $user->email;
+    if (isset($user->subscriptions) && $this->isSubscribed($user->subscriptions) ||
+        (!isset($user->subscriptions))) {
+
+      // @todo: Add email format validation
+      if ($this->isValidEmail($user->email)) {
+        $this->digestUser['email'] = $user->email;
+      }
+      else {
+        echo 'MBP_UserDigest_DirectorProducer->setUser(): No email found, skipping user document.', PHP_EOL;
+        return FALSE;
+      }
+
+      if (isset($user->first_name) && $user->first_name != '') {
+        $this->digestUser['first_name'] = $user->first_name;
+      }
+      else {
+        echo 'MBP_UserDigest_DirectorProducer->setUser(): Using default first name for ' . $user->email, PHP_EOL;
+        // Some PHP OO funyiness: http://stackoverflow.com/questions/5447541/accessing-php-class-constants
+        $tempToolbox = $this->toolbox;
+        $this->digestUser['first_name'] = $tempToolbox::DEFAULT_FIRST_NAME;
+      }
+
+      if (count($user->campaigns) > 0) {
+        $this->digestUser['campaigns'] = $this->scrubCampaigns($user->campaigns);
+      }
+      else {
+        echo 'MBP_UserDigest_DirectorProducer->setUser(): no campaigns found for ' . $user->email, PHP_EOL;
+        return FALSE;
+      }
+
+      if (isset($user->drupal_uid)) {
+        $this->digestUser['drupal_uid'] = $user->drupal_uid;
+      }
+      else {
+        echo 'MBP_UserDigest_DirectorProducer->setUser(): drupal_uid not set for ' . $user->email, PHP_EOL;
+        return FALSE;
+      }
+
+      return TRUE;
     }
     else {
-      echo 'MBP_UserDigest_DirectorProducer->setUser(): No email found, skipping user document.', PHP_EOL;
       return FALSE;
     }
 
-    if (isset($user->first_name) && $user->first_name != '') {
-      $this->digestUser['first_name'] = $user->first_name;
-    }
-    else {
-      echo 'MBP_UserDigest_DirectorProducer->setUser(): Using default first name for ' . $user->email, PHP_EOL;
-      $this->digestUser['first_name'] = $this->toolbox->DEFAULT_FIRST_NAME;
-    }
-
-    if (count($user->campaigns) > 0) {
-      $this->digestUser['campaigns'] = $this->scrubCampaigns($user->campaigns);
-    }
-    else {
-      echo 'MBP_UserDigest_DirectorProducer->setUser(): no campaigns found for ' . $user->email, PHP_EOL;
-      return FALSE;
-    }
-
-    if (isset($user->drupal_uid)) {
-      $this->digestUser['drupal_uid'] = $user->drupal_uid;
-    }
-    else {
-      echo 'MBP_UserDigest_DirectorProducer->setUser(): drupal_uid not set for ' . $user->email, PHP_EOL;
-      return FALSE;
-    }
-
-    return TRUE;
   }
 
   /**
@@ -95,65 +105,105 @@ class MBP_UserDigest_DirectorProducer extends MBP_UserDigest_BaseProducer
    */
   private function isValidEmail($email) {
 
-  /*
-
-    isset($user->email) && $user->email != ''
-    if (isset($userApiResult->email) && (strpos($userApiResult->email, '@mobile') === FALSE ||
-    strlen(substr($userApiResult->email, strpos($userApiResult->email, '@mobile'))) > 7)) {
-
-    */
+    // Test 1
+    if (isset($email) && $email == '' && filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+      echo 'MBP_UserDigest_DirectorProducer->isValidEmail() failed test 1: ' . $email, PHP_EOL;
+      return FALSE;
+    }
+    // Test 2
+    if (isset($email) && strlen(substr($email, strpos($email, '@mobile'))) <= 7) {
+      echo 'MBP_UserDigest_DirectorProducer->isValidEmail() failed test 2: ' . $email, PHP_EOL;
+      return FALSE;
+    }
+    // Test 3
+    elseif (isset($email) && strpos($email, '@mobile') != FALSE) {
+      echo 'MBP_UserDigest_DirectorProducer->isValidEmail() failed test 3: ' . $email, PHP_EOL;
+      return FALSE;
+    }
 
     return TRUE;
   }
 
   /**
-   * isSubscribed:
+   * isSubscribed(): Evaluate user subscription settings including the lack of a preference.
+   *
+   * @param object $subscriptions
+   *   All of the user subscription settings based on user preferences managed at
+   *   subscriptions.dosomething.org.
    */
   private function isSubscribed($subscriptions) {
 
-    /*
+    // Exclude users who have have been banned OR no preference has been set for banning
+    if ( isset($subscriptions) && !isset($subscriptions->banned) ) {
 
-                // Exclude users who have have been banned OR no preference has been set for banning
-              if ( isset($userApiResult->subscriptions) && !isset($userApiResult->subscriptions->banned) ) {
+      // Include users who have no digest unsubscription setting or the subscription for digest
+      // messages is true.
+      if ( (!isset($subscriptions->digest)) ||
+           (isset($subscriptions->digest) && $userApiResult->subscriptions->digest == TRUE) ) {
+        return TRUE;
+      }
+      // Exclude users who have unsubscribed from Digest messages
+      elseif (isset($subscriptions->digest)) {
+        return FALSE;
+      }
 
-                // Exclude users who have unsubscribed from Digest messages OR no preference has been set of digest
-                if ( (!isset($userApiResult->subscriptions->digest)) ||
-                     (isset($userApiResult->subscriptions->digest) && $userApiResult->subscriptions->digest == TRUE) ) {
-
-  */
-
+    }
+    elseif (isset($subscriptions->banned) && $subscriptions->banned == TRUE) {
+      return FALSE;
+    }
     return TRUE;
   }
 
   /**
+   * scrubCampaigns() : Check for required campaign nid and remove campaigns that have been completed.
+   * Compleation is defined as a campaign that has been signed up for and a report as been completed.
    *
+   * @param array $campaigns
+   *   A list of all the campaigns a user has signed up for and reported back on.
+   *
+   * @return array $cleanCampaigns
+   *   A list of all of the user campaigns that are eligible for digest content processing.
    */
   private function scrubCampaigns($campaigns) {
 
-    /*
+    $cleanCampaigns = array();
+    foreach ($campaigns as $campaignCount => $campaign) {
 
-                       $campaigns = array();
-                  foreach ($userApiResult->campaigns as $campaignCount => $campaign) {
-                    if (isset($campaign->nid)) {
-                      $campaigns[$campaignCount] = array(
-                        'nid' => $campaign->nid
-                      );
-                      if (isset($campaign->signup)) {
-                        $campaigns[$campaignCount]['signup'] = strtotime($campaign->signup);
-                      }
-                      if (isset($campaign->reportback)) {
-                        $campaigns[$campaignCount]['reportback'] = strtotime($campaign->reportback);
-                      }
-                    }
-                    else {
-                      echo 'Missing campaign activity nid!', PHP_EOL;
-                      echo('<pre>' . print_r($userApiResult, TRUE) . '</pre>');
-                    }
-                  }
+      if (isset($campaign->nid)) {
 
-    */
+        $cleanCampaigns[$campaignCount] = array(
+          'nid' => $campaign->nid
+        );
 
-    return $campaigns;
+        if (isset($campaign->signup)) {
+          $cleanCampaigns[$campaignCount]['signup'] = strtotime($campaign->signup);
+          $signupFound = TRUE;
+        }
+        else {
+          $signupFound = FALSE;
+        }
+
+        if (isset($campaign->reportback)) {
+          $cleanCampaigns[$campaignCount]['reportback'] = strtotime($campaign->reportback);
+          $reportbackFound = TRUE;
+        }
+        else {
+          $reportbackFound = FALSE;
+        }
+
+        // Remove campaign activity that's complete
+        if (($signupFound && $reportbackFound) || (!$signupFound && $reportbackFound)) {
+          unset($cleanCampaigns[$campaignCount]);
+        }
+
+      }
+      else {
+        echo 'MBP_UserDigest_DirectorProducer->scrubCampaigns(): Missing campaign activity nid: ' . print_r($cmmpaign), PHP_EOL;
+      }
+
+    }
+
+    return $cleanCampaigns;
   }
 
 }
